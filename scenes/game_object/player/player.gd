beaf099951e6d851ebc632e94b1dce36b224f5ed
Onur Ucar <hello@ucaronur.com>
2024-05-24 14:53:00 -0400

    Implemented the initial health system along with a simple health bar representation

--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -1,7 +1,21 @@
 extends CharacterBody2D
 
+@onready var damage_interval_timer: Timer = $DamageIntervalTimer
+@onready var health_component: HealthComponent = $HealthComponent
+@onready var health_bar: ProgressBar = $HealthBar
+@onready var collision_area: Area2D = $CollisionArea
+
 const MAX_SPEED: int = 125
 const ACCELERATION_SMOOTHING: int = 25
+var colliding_bodies_quantity: int = 0
+
+
+func _ready():
+	collision_area.body_entered.connect(on_body_entered)
+	collision_area.body_exited.connect(on_body_exited)
+	damage_interval_timer.timeout.connect(on_damage_interval_timer_timeout)
+	health_component.health_changed.connect(on_health_changed)
+	update_health_display()
 
 
 func _process(delta):
@@ -10,3 +24,30 @@ func _process(delta):
 	velocity = velocity.lerp(target_velocity, 1 - exp(-delta * ACCELERATION_SMOOTHING))
 	move_and_slide()
 
+
+func check_deal_damage():
+	var isDamageTimerActive = ! damage_interval_timer.is_stopped()
+	if colliding_bodies_quantity == 0 || isDamageTimerActive: return
+	health_component.damage(1)
+	damage_interval_timer.start()
+
+
+func update_health_display():
+	health_bar.value = health_component.get_health_percent()
+
+
+func on_body_entered(_other_body: Node2D):
+	colliding_bodies_quantity += 1
+	check_deal_damage()
+
+
+func on_body_exited(_other_body: Node2D):
+	colliding_bodies_quantity -= 1
+
+
+func on_damage_interval_timer_timeout():
+	check_deal_damage()
+
+
+func on_health_changed():
+	update_health_display()
--- upgrades/ability_upgrade.gd
+++
@@ -1,7 +0,0 @@
-extends Resource
-class_name AbilityUpgrade
-
-@export var id: String
-@export var name: String
-@export_multiline var description: String
-
