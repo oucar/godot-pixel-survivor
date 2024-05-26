92ff47f6a9743ba91b79bb4bbde5b4ee07d264e8
Onur Ucar <hello@ucaronur.com>
2024-05-26 07:53:22 -0400

    Added a new ability for axes, enabled axe acquisition through upgrade menu and added max qty ability

--- scenes/manager/upgrade_manager.gd
+++ scenes/manager/upgrade_manager.gd
@@ -8,30 +8,45 @@ var current_upgrades = {}
 
 func _ready():
 	experience_manager.level_up.connect(on_levep_up)
-
-
-func on_levep_up(_current_level: int):
-	var chosen_upgrade = upgrade_pool.pick_random() as AbilityUpgrade
-	if chosen_upgrade == null: return
-	var upgrade_scene_instance = upgrade_screen_scene.instantiate()
-	add_child(upgrade_scene_instance)
-	var chosen_upgrade_array = [chosen_upgrade] as Array[AbilityUpgrade]
-	upgrade_scene_instance.set_ability_upgrade(chosen_upgrade_array)
-	upgrade_scene_instance.upgrade_selected.connect(on_upgrade_selected)
-
+	
 
 func apply_upgrade(upgrade: AbilityUpgrade):
 	var has_upgrade = current_upgrades.has(upgrade.id)
-	if ! has_upgrade:
+	if !has_upgrade:
 		current_upgrades[upgrade.id] = {
 			"resource": upgrade,
 			"quantity": 1
 		}
-	else:
-		current_upgrades[upgrade.id]["quantity"] += 1
+	else: current_upgrades[upgrade.id]["quantity"] += 1
+	
+	if upgrade.max_quantity > 0:
+		var current_quantity = current_upgrades[upgrade.id]["quantity"]
+		if current_quantity == upgrade.max_quantity:
+			upgrade_pool = upgrade_pool.filter(func (pool_upgrade): return pool_upgrade.id != upgrade.id)
 
 	GameEvents.emit_ability_upgrade_added(upgrade, current_upgrades)
 
 
+func pick_upgrades():
+	var chosen_upgrades: Array[AbilityUpgrade] = []
+	var filtered_upgrades = upgrade_pool.duplicate()
+	var upgrades_quantity = filtered_upgrades.size()
+	for i in upgrades_quantity:
+		if upgrades_quantity == 0: break
+		var chosen_upgrade = filtered_upgrades.pick_random() as AbilityUpgrade
+		chosen_upgrades.append(chosen_upgrade)
+		filtered_upgrades = filtered_upgrades.filter(func (upgrade): return upgrade.id != chosen_upgrade.id)
+	
+	return chosen_upgrades
+		
+
 func on_upgrade_selected(upgrade: AbilityUpgrade):
 	apply_upgrade(upgrade)
+
+
+func on_levep_up(_current_level: int):
+	var upgrade_scene_instance = upgrade_screen_scene.instantiate()
+	add_child(upgrade_scene_instance)
+	var chosen_upgrades = pick_upgrades()
+	upgrade_scene_instance.set_ability_upgrade(chosen_upgrades as Array[AbilityUpgrade])
+	upgrade_scene_instance.upgrade_selected.connect(on_upgrade_selected)
