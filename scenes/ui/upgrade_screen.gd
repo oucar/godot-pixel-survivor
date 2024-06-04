e38dd6217f7162cb4a59b163e77257aee897cb83
Onur Ucar <hello@ucaronur.com>
2024-06-03 22:27:17 -0400

    Added card components with animations for the upgradable abilitiies

--- scenes/ui/upgrade_screen.gd
+++ scenes/ui/upgrade_screen.gd
@@ -3,6 +3,7 @@ extends CanvasLayer
 signal upgrade_selected(upgrade: AbilityUpgrade)
 
 @export var upgrade_card_scene: PackedScene
+@onready var animation_player = $AnimationPlayer
 @onready var card_container: HBoxContainer = %CardContainer
 
 
@@ -11,14 +12,19 @@ func _ready():
 
 
 func set_ability_upgrade(upgrades: Array[AbilityUpgrade]):
+	var delay = 0
 	for upgrade in upgrades:
 		var card_instance = upgrade_card_scene.instantiate()
 		card_container.add_child(card_instance)
 		card_instance.set_ability_upgrade(upgrade)
+		card_instance.play_in(delay)
 		card_instance.selected.connect(on_upgrade_selected.bind(upgrade))
+		delay += 0.2
 
 
 func on_upgrade_selected(upgrade: AbilityUpgrade):
 	upgrade_selected.emit(upgrade)
+	animation_player.play("out")
+	await animation_player.animation_finished
 	get_tree().paused = false
 	queue_free()
